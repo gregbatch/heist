4133aff629a1b247bceaaa976b13d287b3c42481
gregbatch <120431206+gregbatch@users.noreply.github.com>
2023-02-27 21:59:55 -0500

    Draft of final project

---
+++ app.js
@@ -0,0 +1,70 @@
+// const artOne = document.getElementById('surrealism');
+// const artTwo = document.getElementById('impressionism');
+// const artThree = document.getElementById('modern');
+// const artFour = document.getElementById('ancient');
+// const artFive = document.getElementById('expressionism');
+// const show = document.querySelector('#show')
+
+// artOne.addEventListener('click', nextArt);
+// artTwo.addEventListener('click', nextArt);
+// artThree.addEventListener('click', nextArt);
+// artFour.addEventListener('click', nextArt);
+// artFive.addEventListener('click', nextArt);
+
+
+// let currentArt;
+
+// function nextArt(click){
+//     currentArt = click.target.id;
+//     if(currentArt==='surrealism'){
+//         show.src=imageOne;
+//     }
+//     else if(currentArt==='impressionism'){
+//         show.src=imageTwo;
+//     }
+//     else if(currentArt==='modern'){
+//         show.src=imageThree;
+//     }
+//     else if(currentArt==='expressionism'){
+//         show.src=imageFive;
+//     }
+// }
+
+
+const images = [
+    "https://upload.wikimedia.org/wikipedia/commons/5/54/Claude_Monet%2C_Impression%2C_soleil_levant.jpg",
+    "https://upload.wikimedia.org/wikipedia/commons/5/54/Claude_Monet%2C_Impression%2C_soleil_levant.jpg",
+    "https://collectionapi.metmuseum.org/api/collection/v1/iiif/490018/1009285/restricted",
+    "https://upload.wikimedia.org/wikipedia/commons/9/9d/The_Scream_by_Edvard_Munch%2C_1893_-_Nasjonalgalleriet.png"
+]
+
+console.log(images.length);
+
+const artNext = document.querySelectorAll(".quiz");
+console.log(artNext);
+
+for (var i = 0; i < artNext.length; i++) {
+    let el = artNext[i];
+    el.addEventListener("click", imageSwitcher);
+}
+
+const artScreen = document.querySelector("#art-screen");
+
+function imageSwitcher(event){
+    let text = event.target.innerText;
+    let index = parseInt(text) - 1;
+    let imgToSet = images[index];
+    artScreen.setAttribute("src", imgToSet);
+}
+
+
+
+//images
+
+// 1 - src=""
+// 2 - src="https://upload.wikimedia.org/wikipedia/commons/5/54/Claude_Monet%2C_Impression%2C_soleil_levant.jpg"
+// 3 - src="https://collectionapi.metmuseum.org/api/collection/v1/iiif/490018/1009285/restricted"
+// 4 - src="https://upload.wikimedia.org/wikipedia/commons/9/9d/The_Scream_by_Edvard_Munch%2C_1893_-_Nasjonalgalleriet.png"
+// 5 - src="""
+
+//
